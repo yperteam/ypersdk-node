1ccb834b8f2051525bf6fe658c068ae6130c22c1
Manuel Trambert <m.trambert@gmail.com>
2019-07-01 16:49:51 +0200

    delete console log

--- service/invoice/read.js
+++ service/invoice/read.js
@@ -3,9 +3,7 @@ import { Request } from '../../request';
 class Invoices {
 
     static async get(api) {
-        console.log('HERE:', api);
         const res = await Request.performRequest('/invoice', 'GET', null, null, api);
-        console.log('RES:', res);
     }
 }
 
